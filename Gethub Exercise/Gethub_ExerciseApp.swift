bdfcc65421a691c556ae8b24d451d81f18c74f2e
Sirreal01 <sirreal01@comcast.net>
2024-11-08 10:48:32 -0500

    Initial Commit

---
+++ Gethub Exercise/Gethub_ExerciseApp.swift
@@ -0,0 +1,17 @@
+//
+//  Gethub_ExerciseApp.swift
+//  Gethub Exercise
+//
+//  Created by ladon brown on 11/8/24.
+//
+
+import SwiftUI
+
+@main
+struct Gethub_ExerciseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
